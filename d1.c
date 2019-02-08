3a35516300f614eeedc45a8ad54da39fabf7c72f
dinesh1601041 <47448835+dinesh1601041@users.noreply.github.com>
2019-02-08 16:52:02 +0530

    Create d1.c

---
+++ d1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==0)
+    printf("zero");
+    else if(a>0)
+    printf("postive");
+    else
+    printf("negative");
+
+    return 0;
+}
